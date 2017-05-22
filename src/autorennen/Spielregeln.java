ec640a313419f3dcb0fd346cac3a2d72e0dbb731
bbsuser <bbsuser@E201-23>
2017-05-22 17:44:57 +0200

    Änderungen Lesch vom 22.05.2017

--- src/autorennen/Spielregeln.java
+++ src/autorennen/Spielregeln.java
@@ -3,16 +3,28 @@ package autorennen;
 public class Spielregeln {
 
     private double faktor;
+    private String hindernis;
 
     public void regen() {
-        this.faktor = 2;
+        this.faktor = 0.75;
+        this.hindernis = "Regen";
     }
-
     public void gegenwind() {
-        this.faktor = 1.5;
+        this.faktor = 0.9;
+        this.hindernis = "Gegenwind";
     }
-
     public double getFaktor() {
         return this.faktor;
     }
-}
+    public String getHindernis() {
+        return this.hindernis;
+    }
+    public void setZufallHindernis() {
+        this.faktor = Math.random();
+        if (this.faktor > 0.89) {
+            this.hindernis = "Wind";
+        } else {
+            this.hindernis = "Regen";
+        }
+    }
+ }
--- src/autorennen/Steuerung.java
+++ src/autorennen/Steuerung.java
@@ -1,16 +1,16 @@
 package autorennen;
 
-
-import autorennen.Auto;
-
 public class Steuerung {
 
     public void gasGeben(Auto auto) {
+        auto.beschleunigen() ;
     }
 
     public void bremsen(Auto auto) {
+        auto.bremsen();
     }
 
     public void handicap(Auto auto) {
+        System.out.println("geht noch nicht ..");
     }
 }
